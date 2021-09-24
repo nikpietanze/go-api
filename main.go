6e38e359687c9289df802c0205a7bd19d08dba1f
Nik Pietanze <nikpietanze@gmail.com>
2021-09-24 14:37:18 -0400

    update to use gin

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
 )
 
 type Book struct {
@@ -25,79 +25,85 @@ type Author struct {
 
 var books []Book
 
-func getBooks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(books)
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
 }
 
-func getBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
+func getBook(c *gin.Context) {
+	requestID := c.Param("id")
+
+	fmt.Println(requestID)
 
 	for _, item := range books {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
+		if item.ID == requestID {
+			c.IndentedJSON(http.StatusOK, item)
 			return
 		}
 	}
-
-	json.NewEncoder(w).Encode(&Book{})
+	c.IndentedJSON(http.StatusNotFound, &Book{})
 }
 
-func createBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+func createBook(c *gin.Context) {
+	var newBook Book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	_ = json.NewDecoder(c.Request.Body).Decode(&newBook)
+	newBook.ID = strconv.Itoa(rand.Intn(10000000))
+	books = append(books, newBook)
 	
-	var book Book
-	_ = json.NewDecoder(r.Body).Decode(&book)
-	book.ID = strconv.Itoa(rand.Intn(10000000))
-	books = append(books, book)
-
-	json.NewEncoder(w).Encode(book)
+	c.IndentedJSON(http.StatusCreated, newBook)
 }
 
-func updateBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range books {
-		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
+func updateBook(c *gin.Context) {
+	requestID := c.Param("id")
+
+	for i, item := range books {
+		if item.ID == requestID {
+			books = append(books[:i], books[i+1:]...)
 			
 			var book Book
-			_ = json.NewDecoder(r.Body).Decode(&book)
-			book.ID = params["id"]
+			if err := c.BindJSON(&book); err != nil {
+				c.Status(http.StatusInternalServerError)
+				return
+			}
+
+			book.ID = requestID
 			books = append(books, book)
-			json.NewEncoder(w).Encode(book)
+			c.IndentedJSON(http.StatusOK, book)
 			return
 		}
 	}
-	json.NewEncoder(w).Encode(books)
+	c.IndentedJSON(http.StatusNotFound, books)
 }
 
-func deleteBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
+func deleteBook(c *gin.Context) {
+	requestID := c.Param("id")
+
 	for index, item := range books {
-		if item.ID == params["id"] {
+		if item.ID == requestID {
 			books = append(books[:index], books[index+1:]...)
 			break
 		}
 	}
-	json.NewEncoder(w).Encode(books)
+	c.IndentedJSON(http.StatusOK, books)
 }
 
 func main() {
-	router := mux.NewRouter()
-
+	router := gin.Default()
+    
 	books = append(books, Book{ID: "1", Isbn: "448743", Title: "Book One", Author: &Author{FirstName: "John", LastName: "Doe"}})
 	books = append(books, Book{ID: "2", Isbn: "234908", Title: "Book Two", Author: &Author{FirstName: "Jane", LastName: "Doe"}})
 
-	router.HandleFunc("/api/books", getBooks).Methods("GET")
-	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	router.HandleFunc("/api/books", createBook).Methods("POST")
-	router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
-	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+	router.GET("/api/books", getBooks)
+	router.GET("/api/books/:id", getBook)
+	router.POST("/api/books", createBook)
+	router.PUT("/api/books/:id", updateBook)
+	router.DELETE("/api/books/:id", deleteBook)
 
 	port := ":8000"
-	fmt.Println("Server started on http://localhost" + port)
-	log.Fatal(http.ListenAndServe(port, router))
+	log.Fatal(router.Run("localhost" + port))
 }
